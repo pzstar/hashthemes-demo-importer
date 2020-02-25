abade0cbeb445bdf00a70e76905419797612ae1d
prabin <prabin@localhost>
2020-02-25 17:19:52 +0545

    Merge origin/master
    Conflicts:
    	.DS_Store

--- import_config.php
+++ import_config.php
@@ -19,13 +19,13 @@ $square = array(
     ),
 );
 
-$square = array(
+$squarepress = array(
     'main' => array(
         'slug' => 'main',
-        'name' => 'Square',
-        'external_url' => 'https://hashthemes.com/import-files/square/main.zip',
-        'image' => 'https://i0.wp.com/themes.svn.wordpress.org/square/1.6.2/screenshot.png',
-        'preview_url' => 'https://demo.hashthemes.com/square',
+        'name' => 'SquarePress',
+        'external_url' => 'https://hashthemes.com/import-files/squarepress/main.zip',
+        'image' => 'https://i0.wp.com/themes.svn.wordpress.org/squarepress/1.0.4/screenshot.png',
+        'preview_url' => 'https://demo.hashthemes.com/squarepress',
         'menu_array' => array(
             'primary' => 'Primary Menu'
         ),
@@ -34,227 +34,47 @@ $square = array(
     )
 );
 
-$demos = array(
-    'magazine' => array(
-        'slug' => 'magazine',
-        'name' => 'Magazine',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/magazine.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/magazine.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/magazine/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
+$total = array(
+    'main' => array(
+        'slug' => 'main',
+        'name' => 'Total',
+        'external_url' => 'https://hashthemes.com/import-files/total/main.zip',
+        'image' => 'https://i0.wp.com/themes.svn.wordpress.org/total/1.2.32/screenshot.png',
+        'preview_url' => 'https://demo.hashthemes.com/total',
         'menu_array' => array(
             'primary' => 'Primary Menu'
         ),
         'home_slug' => 'home',
         'blog_slug' => 'blog'
-    ),
-    'news' => array(
-        'slug' => 'news',
-        'name' => 'News',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/news.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/news.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/news/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
+    )
+);
+
+$totally = array(
+    'main' => array(
+        'slug' => 'main',
+        'name' => 'Totally',
+        'external_url' => 'https://hashthemes.com/import-files/totally/main.zip',
+        'image' => 'https://i0.wp.com/themes.svn.wordpress.org/totally/1.0.9/screenshot.png',
+        'preview_url' => 'https://demo.hashthemes.com/totally',
         'menu_array' => array(
             'primary' => 'Primary Menu'
         ),
-        'home_slug' => 'home',
+        'home_slug' => 'home-page',
         'blog_slug' => 'blog'
-    ),
-    'sports' => array(
-        'slug' => 'sports',
-        'name' => 'Sports',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/sports.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/sports.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/sports/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
+    )
+);
+
+$viral = array(
+    'main' => array(
+        'slug' => 'main',
+        'name' => 'Totally',
+        'external_url' => 'https://hashthemes.com/import-files/viral/main.zip',
+        'image' => 'https://i0.wp.com/themes.svn.wordpress.org/viral/1.4.2/screenshot.png',
+        'preview_url' => 'https://demo.hashthemes.com/viral',
         'menu_array' => array(
             'primary' => 'Primary Menu'
         ),
-        'home_slug' => 'home',
-        'blog_slug' => 'blog'
-    ),
-    'technology' => array(
-        'slug' => 'technology',
-        'name' => 'Technology',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/technology.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/technology.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/technology/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
-        'menu_array' => array(
-            'primary' => 'Primary Menu'
-        ),
-        'home_slug' => 'home',
-        'blog_slug' => 'blog'
-    ),
-    'illustration' => array(
-        'slug' => 'illustration',
-        'name' => 'Illustration',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/illustration.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/illustration.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/illustration/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
-        'menu_array' => array(
-            'primary' => 'Primary Menu'
-        ),
-        'home_slug' => 'home',
-        'blog_slug' => 'blog'
-    ),
-    'fashion' => array(
-        'slug' => 'fashion',
-        'name' => 'Fashion',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/fashion.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/fashion.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/fashion/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
-        'menu_array' => array(
-            'primary' => 'Primary Menu'
-        ),
-        'home_slug' => 'home',
-        'blog_slug' => 'blog'
-    ),
-    'travel' => array(
-        'slug' => 'travel',
-        'name' => 'Travel',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/travel.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/travel.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/travel/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
-        'menu_array' => array(
-            'primary' => 'Primary Menu'
-        )
-    ),
-    'rtl' => array(
-        'slug' => 'rtl',
-        'name' => 'RTL',
-        'external_url' => 'https://hashthemes.com/import-files/viral-pro/rtl.zip',
-        'image' => 'https://hashthemes.com/import-files/viral-pro/screen/rtl.jpg',
-        'preview_url' => 'https://demo.hashthemes.com/viral-pro/rtl/',
-        'tags' => array(
-            'creative' => 'Creative',
-            'portfolio' => 'Portfolio',
-        ),
-        'plugins' => array(
-            'social-count-plus' => array(
-                'name' => 'Social Count Plus',
-                'source' => 'wordpress',
-                'file_path' => 'social-count-plus/social-count-plus.php'
-            ),
-            'instagram-feed' => array(
-                'name' => 'Smash Balloon Instagram Feed',
-                'source' => 'wordpress',
-                'file_path' => 'instagram-feed/instagram-feed.php'
-            )
-        ),
-        'menu_array' => array(
-            'primary' => 'Primary Menu'
-        ),
-        'home_slug' => 'home',
+        'home_slug' => 'home-page',
         'blog_slug' => 'blog'
     )
 );
